243311f3bab0480e4f315628c1c0dee0c7f9f9c1
Zhang Junnan <zhangjn0430@gmail.com>
2014-02-20 17:35:36 +0800

    Add Home controller

--- MusicStore/Modules/MusicStore/Models/Album.cs
+++ MusicStore/Modules/MusicStore/Models/Album.cs
@@ -31,8 +31,8 @@ namespace MusicStore.Models
         [StringLength(1024)]
         public string AlbumArtUrl { get; set; }
 
-        //public virtual Genre Genre { get; set; }
-        //public virtual Artist Artist { get; set; }
-        //public virtual List<OrderDetail> OrderDetails { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
+        public virtual List<OrderDetail> OrderDetails { get; set; }
     }
 }
