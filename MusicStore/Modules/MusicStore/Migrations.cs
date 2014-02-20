3d514a7902b505e1bad4900492e36c6050b7337f
sword <caoxiangkun@gmail.com>
2014-02-20 14:21:35 +0800

    add Album and Artist model

--- MusicStore/Modules/MusicStore/Migrations.cs
+++ MusicStore/Modules/MusicStore/Migrations.cs
@@ -1,4 +1,5 @@
-﻿using Coevery.ContentManagement.MetaData;
+﻿using System.Data;
+using Coevery.ContentManagement.MetaData;
 using Coevery.Data.Migration;
 
 namespace MusicStore
@@ -7,7 +8,26 @@ namespace MusicStore
     {
         public int Create()
         {
+            SchemaBuilder.CreateTable("Album",
+                table => table
+                    .Column<int>("AlbumId", col => col.PrimaryKey().Identity())
+                    .Column<int>("GenreId", col => col.NotNull())
+                    .Column<int>("ArtistId", col => col.NotNull())
+                    .Column<string>("Title", col => col.WithLength(160))
+                    .Column<double>("Price", col => col.WithPrecision(10).WithScale(2))
+                    .Column<string>("AlbumArtUrl",
+                        col => col.WithLength(1024).WithDefault("/Content/Images/placeholder.gif"))
+                    .Column<string>("PasswordSalt")
+                    .Column<string>("RegistrationStatus", c => c.WithDefault("Approved"))
+                    .Column<string>("EmailStatus", c => c.WithDefault("Approved"))
+                    .Column<string>("EmailChallengeToken")
+                );
 
+            SchemaBuilder.CreateTable("Artist",
+                table => table
+                    .Column<int>("ArtistId", col => col.PrimaryKey().Identity())
+                    .Column<string>("Name", col => col.WithLength(120))
+                );
 
             return 1;
         }
