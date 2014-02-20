7b7c8112c8b527cca2160b79ae0aeca8af012a09
sword <caoxiangkun@gmail.com>
2014-02-20 14:01:41 +0800

    migration

--- MusicStore/Modules/MusicStore/Migrations.cs
+++ MusicStore/Modules/MusicStore/Migrations.cs
@@ -1,26 +1,15 @@
 ﻿using Coevery.ContentManagement.MetaData;
 using Coevery.Data.Migration;
 
-namespace MusicStore {
-    public class MusicStoreDataMigration : DataMigrationImpl {
+namespace MusicStore
+{
+    public class MusicStoreDataMigration : DataMigrationImpl
+    {
+        public int Create()
+        {
 
-        //public int Create() {
-        //    SchemaBuilder.CreateTable("UserRecord", 
-        //        table => table
-        //            .ContentPartRecord()
-        //            .Column<string>("UserName")
-        //            .Column<string>("Email")
-        //            .Column<string>("NormalizedUserName")
-        //            .Column<string>("Password")
-        //            .Column<string>("PasswordFormat")
-        //            .Column<string>("HashAlgorithm")
-        //            .Column<string>("PasswordSalt")
-        //            .Column<string>("RegistrationStatus", c => c.WithDefault("Approved"))
-        //            .Column<string>("EmailStatus", c => c.WithDefault("Approved"))
-        //            .Column<string>("EmailChallengeToken")
-        //        );
 
-        //    return 1;
-        //}
+            return 1;
+        }
     }
 }
