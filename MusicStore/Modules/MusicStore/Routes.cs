d8766e418caebae50cbc82ffa32f188eb3107680
Zhang Junnan <zhangjn0430@gmail.com>
2014-02-21 15:19:52 +0800

    Add default route

---
+++ MusicStore/Modules/MusicStore/Routes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Coevery.Mvc.Routes;
+
+namespace MusicStore {
+    public class Routes : IRouteProvider {
+        public IEnumerable<RouteDescriptor> GetRoutes() {
+            return new[] {
+                new RouteDescriptor {
+                    Priority = 100,
+                    Route = new Route("",
+                        new RouteValueDictionary {
+                            {"area", "MusicStore"},
+                            {"controller", "Home"},
+                            {"action", "Index"}
+                        }, new RouteValueDictionary(),
+                        new RouteValueDictionary {{"area", "MusicStore"}},
+                        new MvcRouteHandler())
+                }
+            };
+        }
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes) {
+            foreach(RouteDescriptor route in GetRoutes()) {
+                routes.Add(route);
+            }
+        }
+    }
+}
