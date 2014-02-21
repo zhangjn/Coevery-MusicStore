e701d99f72cb3ea2c8c90e8ca5689e616fb7fc03
Zhang Junnan <zhangjn0430@gmail.com>
2014-02-21 14:10:13 +0800

    Update theme

--- MusicStore/Modules/MusicStore/Models/ShoppingCart.cs
+++ MusicStore/Modules/MusicStore/Models/ShoppingCart.cs
@@ -5,40 +5,33 @@ using System.Web;
 using System.Web.Mvc;
 using Coevery.Data;
 
-namespace MusicStore.Models
-{
-    public partial class ShoppingCart
-    {
+namespace MusicStore.Models {
+    public class ShoppingCart {
         private readonly IRepository<Cart> _cartRepository;
         private readonly IRepository<OrderDetail> _orderDetailRepository;
 
-        string ShoppingCartId { get; set; }
+        private string ShoppingCartId { get; set; }
 
         public const string CartSessionKey = "CartId";
 
-        public static ShoppingCart GetCart(HttpContextBase context)
-        {
+        public static ShoppingCart GetCart(HttpContextBase context) {
             var cart = new ShoppingCart();
             cart.ShoppingCartId = cart.GetCartId(context);
             return cart;
         }
 
         // Helper method to simplify shopping cart calls
-        public static ShoppingCart GetCart(Controller controller)
-        {
+        public static ShoppingCart GetCart(Controller controller) {
             return GetCart(controller.HttpContext);
         }
 
-        public void AddToCart(Album album)
-        {
+        public void AddToCart(Album album) {
             // Get the matching cart and album instances
             var cartItem = _cartRepository.Table.SingleOrDefault(c => c.Cart_Id == ShoppingCartId && c.Album_Id == album.Id);
 
-            if (cartItem == null)
-            {
+            if(cartItem == null) {
                 // Create a new cart item if no cart item exists
-                cartItem = new Cart
-                {
+                cartItem = new Cart {
                     Album_Id = album.Id,
                     Cart_Id = ShoppingCartId,
                     Count = 1,
@@ -47,32 +40,26 @@ namespace MusicStore.Models
 
                 _cartRepository.Create(cartItem);
             }
-            else
-            {
+            else {
                 // If the item does exist in the cart, then add one to the quantity
                 cartItem.Count++;
             }
-
         }
 
-        public int RemoveFromCart(int id)
-        {
+        public int RemoveFromCart(int id) {
             // Get the cart
             var cartItem = _cartRepository.Table.Single(
-                    cart => cart.Cart_Id == ShoppingCartId
-                    && cart.Id == id);
+                cart => cart.Cart_Id == ShoppingCartId
+                        && cart.Id == id);
 
             int itemCount = 0;
 
-            if (cartItem != null)
-            {
-                if (cartItem.Count > 1)
-                {
+            if(cartItem != null) {
+                if(cartItem.Count > 1) {
                     cartItem.Count--;
                     itemCount = cartItem.Count;
                 }
-                else
-                {
+                else {
                     _cartRepository.Delete(cartItem);
                 }
             }
@@ -80,54 +67,46 @@ namespace MusicStore.Models
             return itemCount;
         }
 
-        public void EmptyCart()
-        {
+        public void EmptyCart() {
             var cartItems = _cartRepository.Table.Where(cart => cart.Cart_Id == ShoppingCartId);
 
-            foreach (var cartItem in cartItems)
-            {
+            foreach(var cartItem in cartItems) {
                 _cartRepository.Delete(cartItem);
             }
         }
 
-        public List<Cart> GetCartItems()
-        {
+        public List<Cart> GetCartItems() {
             return _cartRepository.Table.Where(cart => cart.Cart_Id == ShoppingCartId).ToList();
         }
 
-        public int GetCount()
-        {
+        public int GetCount() {
             // Get the count of each item in the cart and sum them up
             int? count = (from cartItems in _cartRepository.Table
-                          where cartItems.Cart_Id == ShoppingCartId
-                          select (int?)cartItems.Count).Sum();
+                where cartItems.Cart_Id == ShoppingCartId
+                select (int?)cartItems.Count).Sum();
 
             // Return 0 if all entries are null
             return count ?? 0;
         }
 
-        public decimal GetTotal()
-        {
+        public decimal GetTotal() {
             // Multiply album price by count of that album to get 
             // the current price for each of those albums in the cart
             // sum all album price totals to get the cart total
             decimal? total = (from cartItems in _cartRepository.Table
-                              where cartItems.Cart_Id == ShoppingCartId
-                              select (int?)cartItems.Count * cartItems.Album.Price).Sum();
+                where cartItems.Cart_Id == ShoppingCartId
+                select (int?)cartItems.Count * cartItems.Album.Price).Sum();
             return total ?? decimal.Zero;
         }
 
-        public int CreateOrder(Order order)
-        {
+        public int CreateOrder(Order order) {
             decimal orderTotal = 0;
 
             var cartItems = GetCartItems();
 
             // Iterate over the items in the cart, adding the order details for each
-            foreach (var item in cartItems)
-            {
-                var orderDetail = new OrderDetail
-                {
+            foreach(var item in cartItems) {
+                var orderDetail = new OrderDetail {
                     Album_Id = item.Album_Id,
                     Order_Id = order.Id,
                     UnitPrice = item.Album.Price,
@@ -138,7 +117,6 @@ namespace MusicStore.Models
                 orderTotal += (item.Count * item.Album.Price);
 
                 _orderDetailRepository.Create(orderDetail);
-
             }
 
             // Set the order's total to the orderTotal count
@@ -152,16 +130,12 @@ namespace MusicStore.Models
         }
 
         // We're using HttpContextBase to allow access to cookies.
-        public string GetCartId(HttpContextBase context)
-        {
-            if (context.Session[CartSessionKey] == null)
-            {
-                if (!string.IsNullOrWhiteSpace(context.User.Identity.Name))
-                {
+        public string GetCartId(HttpContextBase context) {
+            if(context.Session[CartSessionKey] == null) {
+                if(!string.IsNullOrWhiteSpace(context.User.Identity.Name)) {
                     context.Session[CartSessionKey] = context.User.Identity.Name;
                 }
-                else
-                {
+                else {
                     // Generate a new random GUID using System.Guid class
                     Guid tempCartId = Guid.NewGuid();
 
@@ -175,12 +149,10 @@ namespace MusicStore.Models
 
         // When a user has logged in, migrate their shopping cart to
         // be associated with their username
-        public void MigrateCart(string userName)
-        {
+        public void MigrateCart(string userName) {
             var shoppingCart = _cartRepository.Table.Where(c => c.Cart_Id == ShoppingCartId);
 
-            foreach (Cart item in shoppingCart)
-            {
+            foreach(Cart item in shoppingCart) {
                 item.Cart_Id = userName;
             }
         }
